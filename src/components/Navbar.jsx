f00672e75d41c27803274f7295bdcefbd7e22ba6
Anastasiia Petrova <paa.front@gmail.com>
2022-09-16 21:53:12 +0300

    sn-1

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Navbar = () => {
+  return(
+    <nav className="nav"> 
+      <a>Profile</a>
+      <a>Messages</a>
+      <a>News</a>
+      <a>Music</a>
+      <a>Settings</a>
+    </nav>
+  );
+}
+
+export default Navbar;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+const Profile = () => {
+  return(
+    <div className="content">
+      <div>
+        <img src="https://vjoy.cc/wp-content/uploads/2019/07/1-1.jpg" />
+      </div>
+      <div>
+        ava+desc
+      </div>
+      <div>
+        nev post
+      </div>
+      <div>
+        posts
+      </div>
+    </div>
+  );
+}
+
+export default Profile;

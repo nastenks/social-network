cf8a789b1108d3ab15a808eb79dd33cad2af5952
Anastasiia Petrova <paa.front@gmail.com>
2022-09-21 20:58:01 +0300

    sn-10 React.createRef

--- src/components/Profile/MyPosts/NewPost/NewPost.jsx
+++ src/components/Profile/MyPosts/NewPost/NewPost.jsx
@@ -2,13 +2,21 @@ import React from "react";
 import s from "./NewPost.module.css"
 
 const NewPost = () => {
+
+  let textNewPost = React.createRef();
+
+  let addPost = () => { 
+    let text = textNewPost.current.value;
+    alert(text);
+  }
+
   return(
     <div className={s.new_post}>
       <div className={s.avatar}>
         <img src="https://drasler.ru/wp-content/uploads/2019/05/%D0%90%D0%B2%D0%B0-%D0%B4%D0%BB%D1%8F-%D0%BF%D0%B0%D1%80%D0%BD%D1%8F-%D0%BF%D1%80%D0%B8%D0%BA%D0%BE%D0%BB%D1%8C%D0%BD%D0%B0%D1%8F-%D0%B8-%D0%BA%D1%80%D1%83%D1%82%D0%B0%D1%8F_021.jpg" />
       </div>
-      <textarea defaultValue="Write"></textarea>
-      <button>Send</button>
+      <textarea placeholder="Write" ref={textNewPost}></textarea>
+      <button onClick={ addPost }>Send</button>
     </div>
   );
 }
--- src/redux/state.js
+++ src/redux/state.js
@@ -13,9 +13,9 @@ let state = {
       {id: 3, name: 'Kseniya', img: "https://weblinks.ru/wp-content/uploads/2022/02/Krasivye-kartinki-leta-na-zastavku-telefona-1.jpg"}
     ],
     messageData: [
-      {id: 1, message: 'hi'},
-      {id: 2, message: 'hihu'},
-      {id: 3, message: 'how are you?'}
+      {id: 1, message: 'hi', img: "https://avatarko.ru/img/kartinka/33/multfilm_lyagushka_32117.jpg"},
+      {id: 2, message: 'hihu', img: "https://drasler.ru/wp-content/uploads/2019/05/%D0%9A%D0%B0%D1%80%D1%82%D0%B8%D0%BD%D0%BA%D0%B8-%D1%8D%D1%82%D0%BE-%D0%B2%D0%B0%D0%B6%D0%BD%D0%BE-%D0%B7%D0%BD%D0%B0%D1%82%D1%8C-023.jpg"},
+      {id: 3, message: 'how are you?', img: "https://avatarko.ru/img/kartinka/33/multfilm_lyagushka_32117.jpg"}
     ]
   },
   navbarData: {
